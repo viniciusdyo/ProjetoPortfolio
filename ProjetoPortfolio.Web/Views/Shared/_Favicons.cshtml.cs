4d826b08ff1a1d2e298a5109485035d07d0ec049
Vinicius Dyonisio <viniciusdyo@gmail.com>
2023-10-21 19:41:10 -0300

    - Feat - favicon

---
+++ ProjetoPortfolio.Web/Views/Shared/_Favicons.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjetoPortfolio.Web.Views.Shared
+{
+    public class _FaviconsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
